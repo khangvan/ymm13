6925160c108a640dcf479439137895b8f4e01dbd
Van <khang.van@datalogic.com>
2018-02-19 19:37:32 +0700

    data Feb 19 2018

--- data-raw/prep.R
+++ data-raw/prep.R
@@ -1,2 +1,2 @@
-ymm13=read.csv("D:\\RWorking\\datalogicset\\ymm13\\YMM13_Feb2018.csv")
-devtools::use_data(ymm13)
+ymm13.Feb192018=read.csv("D:\\RWorking\\datalogicset\\ymm13\\YMM13_Feb2018.csv")
+devtools::use_data(ymm13.Feb192018)
